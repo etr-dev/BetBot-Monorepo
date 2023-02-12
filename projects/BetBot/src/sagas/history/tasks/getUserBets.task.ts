46339d2bbbe18ae16259f37445185e0d30d2724a
Elijah Robinson <47159753+erobin27@users.noreply.github.com>
2023-02-12 17:02:02 -0500

    update history to fix bug where no bets (#15)

--- projects/BetBot/src/sagas/history/tasks/getUserBets.task.ts
+++ projects/BetBot/src/sagas/history/tasks/getUserBets.task.ts
@@ -2,7 +2,10 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { getUsersBets } from '@apis';
+import { embedCancellation } from '@displayFormatting/cancellation.embed';
+import { messageBuilder } from '@displayFormatting/messageBuilder';
 import { GetUsersBetsRequest } from 'src/apis/backendApi/requests';
+import { TaskError } from 'src/sagas/framework/error';
 import { ITaskData } from 'src/sagas/framework/task';
 
 export async function getUserBetsTask(input: ITaskData): Promise<ITaskData> {
@@ -12,5 +15,19 @@ export async function getUserBetsTask(input: ITaskData): Promise<ITaskData> {
     attachMatchInfo: true,
   };
   const { data } = await getUsersBets(getUsersBetsRequest);
+  if (!data.length) {
+    throw new TaskError('Cancelled History Select.', {
+      interaction: input.interaction,
+      action: 'EDIT',
+      message: messageBuilder({
+        embeds: [
+          embedCancellation(
+            'No Bets',
+            'You currently do not have any bets in the selected category, please use /history to restart.',
+          ),
+        ],
+      }),
+    });
+  }
   return { userBets: data };
 }
