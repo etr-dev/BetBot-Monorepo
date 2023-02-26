e53d6de2895a7075152f2a7d182aa953e8553b14
elijah r <idgnfs@gmail.com>
2023-02-26 15:43:19 -0500

    fix wallet stats not updating

--- projects/BetBot-Backend/src/betbot/betbot.service.ts
+++ projects/BetBot-Backend/src/betbot/betbot.service.ts
@@ -328,6 +328,7 @@ export class BetbotService {
       user.userBets.activeBets.splice(index, 1);
       user.userBets.inactiveBets.push(bet._id);
       user.stats.walletAmount = wallet.amount;
+
       user.stats = this.updateUserStats(user.stats, bet);
 
       const session = await this.connection.startSession();
