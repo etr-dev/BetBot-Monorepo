f0da3d3e8524f7e19faba509da6efdc486abb007
elijah r <idgnfs@gmail.com>
2023-05-28 23:56:58 -0600

    update dtos

--- projects/BetBot-Backend/src/betbot/dto/index.ts
+++ projects/BetBot-Backend/src/betbot/dto/index.ts
@@ -2,3 +2,8 @@ export * from './bet';
 export * from './match';
 export * from './user';
 export * from './wallet';
+
+export * from './bet.dto';
+export * from './match.dto';
+export * from './user.dto';
+export * from './wallet.dto';
--- projects/BetBot-Backend/src/betbot/dto/user/getUser.dto.ts
+++ projects/BetBot-Backend/src/betbot/dto/user/getUser.dto.ts
@@ -1,8 +1,12 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 import { UserDto } from '../user.dto';
 
 export class GetUserDto extends PartialType(UserDto) {
   @IsOptional()
+  @ApiProperty({
+    description: 'sort by a property',
+    example: 'sort[username] = asc',
+  })
   sort?: Record<string, 'asc' | 'desc'> = {};
 }
